db13134ee07e39a0a92d13fc1d44d635fa8419e5
Natalia Marakova <natvldmr@gmail.com>
2023-03-14 22:42:09 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 9765;
+        int bonus = 20;
+
+        System.out.println((price / bonus) + " миль (я)");
+    }
+}
